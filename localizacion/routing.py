7f953c5b3cbca8c862910803e5485aa989b91c1d
Jorge Salgado <salgadojorge@javeriana.edu.co>
2021-11-03 17:30:26 -0500

    removiendo .pyc y eliminando comentarios

--- localizacion/routing.py
+++ localizacion/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 from . import consumers
 
+#Direcciones de los sockets. Segundo argumento es quien consume los mensages (LocConsumer)
 websocket_urlpatterns = [
     re_path(r'ws/loc/biblioteca', consumers.LocConsumer.as_asgi()),
 ]
