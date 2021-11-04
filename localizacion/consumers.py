1232ab5586059b128a7c2a3b40ea8612324062aa
Jorge Salgado <salgadojorge@javeriana.edu.co>
2021-11-03 22:57:36 -0500

    servidor manda localizacion

--- localizacion/consumers.py
+++ localizacion/consumers.py
@@ -1,18 +1,37 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+#from channels.generic.websocket import WebsocketConsumer
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
+from random import choice
+class LocConsumer(AsyncWebsocketConsumer):
+    async def websocket_connect(self, event):
+        self.connect = True
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            "type": "websocket.accept",
+            "message": 'OK'
+            }))
+        
+        asyncio.create_task(self.send_data()) 
 
-class LocConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def websocket_disconnect(self, event):
+        print("disconnected", event)
+        self.connect = False
 
-    def disconnect(self, close_code):
-        pass
+    async def websocket_receive(self, event):
+        print("receive", event)
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
-        if message == 'biblioteca':
-            self.send(text_data=json.dumps({
-                'message': 'Servidor: Te recomendamos estos super libros!!!'
+    async def send_data(self): 
+        lugares:list = ['biblioteca', 'parque', 'aulas', 'cafeteria']
+        while self.connect:
+          await asyncio.sleep(2)
+          obj = choice(lugares)
+          print({
+            'type': 'websocket.send',
+            'message':  obj,
+            })
+          await self.send(text_data=json.dumps({
+                'type': 'websocket.send',
+                'message': obj,
             }))
